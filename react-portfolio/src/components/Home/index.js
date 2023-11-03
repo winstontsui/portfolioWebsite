161ee4dcc41ccd4fc65a9a27c2e2ed0b36acbe60
Winston Tsui <witsui@syr.edu>
2023-11-03 17:42:54 -0400

    Deploy Website to GitHub Pages

--- react-portfolio/src/components/Home/index.js
+++ react-portfolio/src/components/Home/index.js
@@ -1,22 +1,29 @@
 import './index.scss'
 import LogoS from '../../assets/images/logo-s.png'
 import { Link } from 'react-router-dom'
+import AnimatedLetters from '../AnimatedLetters'
+import { useState } from 'react'
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = ['i', 'n', 's', 't', 'o', 'n', '!']
+  const jobArray = ['S', 'o', 'f', 't', 'w', 'a', 'r', 'e', ' ', 'E', 'n', 'g', 'i', 'n', 'e', 'e', 'r']
   return (
     <div className="container home-page">
       <div className="text-zone">
         <h1>
-          Hi, <br /> I'm
+          Hello, <br /> I'm
           <img src={LogoS} alt="developer" />
-          inston
+          <AnimatedLetters letterClass = {letterClass}
+          strArray = {nameArray}
+          idx = {15}/>
           <br />
           Software Engineer
         </h1>
         <h2>
-            Student at Syracuse University / Azure Certified / Software Engineer
+            Student at Syracuse University / Computer Science / Software Engineer
         </h2>
-        <Link to ="/contact" className = 'flat-button'>Contact Me</Link>
+        <Link to ="/contact" className = 'flat-button'>Contact Me Here</Link>
       </div>
     </div>
   )
