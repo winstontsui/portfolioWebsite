fed0478cc185ca65481fa578996fbb40c1fd5781
Winston Tsui <ny1winston@gmail.com>
2024-01-05 16:22:15 -0500

    Removed gsap import, revised About me info

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,6 +1,4 @@
-import { useEffect, useRef } from 'react'
-import gsap from 'gsap-trial'
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
+import { useRef } from 'react'
 import LogoS from '../../../assets/images/logo-s.png'
 import './index.scss'
 
@@ -9,32 +7,6 @@ const Logo = () => {
   const outlineLogoRef = useRef()
   const solidLogoRef = useRef()
 
-  useEffect(() => {
-    gsap.registerPlugin(DrawSVGPlugin)
-
-    gsap
-      .timeline()
-      .to(bgRef.current, {
-        duration: 1,
-        opacity: 1,
-      })
-      .from(outlineLogoRef.current, {
-        drawSVG: '100%',
-        duration: 20,
-      })
-
-    gsap.fromTo(
-      solidLogoRef.current,
-      {
-        opacity: 0,
-      },
-      {
-        opacity: 1,
-        delay: 4,
-        duration: 4,
-      }
-    )
-  }, [])
 
   return (
     <div className="logo-container" ref={bgRef}>
