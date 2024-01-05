71285eef13d5cd68c89d82c0b1389c5dccd73a9b
Winston Tsui <ny1winston@gmail.com>
2024-01-05 04:33:08 -0500

    About me changes, there is still gsap in the logo though.

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,10 +1,16 @@
-import './index.scss'
-import { Link, NavLink } from 'react-router-dom'
-import LogoW from '../../assets/images/logo-w.png'
-import LogoSubtitle from '../../assets/images/logo_sub.png'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
-import { faDiscord, faGithub, faLinkedin, faYoutube } from '@fortawesome/free-brands-svg-icons'
+import './index.scss';
+import { Link, NavLink } from 'react-router-dom';
+import LogoW from '../../assets/images/logo-w.png';
+import LogoSubtitle from '../../assets/images/logo_sub.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import {
+  faFreeCodeCamp,
+  faGithub,
+  faInstagram,
+  faLinkedin,
+  faYoutube,
+} from '@fortawesome/free-brands-svg-icons';
 
 const Sidebar = () => (
   <div className="nav-bar">
@@ -35,30 +41,55 @@ const Sidebar = () => (
       >
         <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
       </NavLink>
-    </nav> 
+    </nav>
     <ul>
-        <li>
-            <a target="_blank" rel = 'noreferrer' href = 'https://www.linkedin.com/in/winstontsui/'>
-                <FontAwesomeIcon icon = {faLinkedin} color = "#4d4d4e"/>
-            </a>
-        </li>
-        <li>
-            <a target="_blank" rel = 'noreferrer' href = 'https://github.com/winston-tsui'>
-                <FontAwesomeIcon icon = {faGithub} color = "#4d4d4e"/>
-            </a>
-        </li>
-        <li>
-            <a target="_blank" rel = 'noreferrer' href = 'https://www.youtube.com/@freecodecamp/videos'>
-                <FontAwesomeIcon icon = {faYoutube} color = "#4d4d4e"/>
-            </a>
-        </li>
-        <li>
-            <a target="_blank" rel = 'noreferrer' href = 'https://discord.com/app'>
-                <FontAwesomeIcon icon = {faDiscord} color = "#4d4d4e"/>
-            </a>
-        </li>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.instagram.com/winstonlovesamongus/"
+        >
+          <FontAwesomeIcon icon={faInstagram} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/winstontsui/"
+        >
+          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/winston-tsui"
+        >
+          <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.youtube.com/@winstont.7305"
+        >
+          <FontAwesomeIcon icon={faYoutube} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.youtube.com/@freecodecamp/videos"
+        >
+          <FontAwesomeIcon icon={faFreeCodeCamp} color="#4d4d4e" />
+        </a>
+      </li>
     </ul>
   </div>
-)
+);
 
-export default Sidebar
+export default Sidebar;
