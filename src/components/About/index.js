3e7162cb717b4863afddfe344adb0c520b2fe402
Winston Tsui <ny1winston@gmail.com>
2024-01-05 03:19:50 -0500

    Changed About me svg

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,22 +1,10 @@
 import './index.scss';
+import concentrationImage from '../../assets/images/Concentration.jpg';
 import AnimatedLetters from '../AnimatedLetters';
-import {
-  faAngular,
-  faCss3,
-  faGitAlt,
-  faHtml5,
-  faJsSquare,
-  faReact,
-} from '@fortawesome/free-brands-svg-icons';
 import { useEffect, useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 import Loader from 'react-loaders';
-import gsap from 'gsap';
 
 const About = () => {
-  gsap.registerPlugin(DrawSVGPlugin);
-
   const [letterClass, setLetterClass] = useState('text-animate');
 
   useEffect(() => {
@@ -65,29 +53,7 @@ const About = () => {
             help and contribute in any way I can.
           </p>
         </div>
-
-        <div className="stage-cube-cont">
-          <div className="cubespinner">
-            <div className="face1">
-              <FontAwesomeIcon icon={faAngular} color="#DD0031" />
-            </div>
-            <div className="face2">
-              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
-            </div>
-            <div className="face3">
-              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
-            </div>
-            <div className="face4">
-              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
-            </div>
-            <div className="face5">
-              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
-            </div>
-            <div className="face6">
-              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
-            </div>
-          </div>
-        </div>
+        <img className = "winstonimage" src={concentrationImage} alt="Exampleeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" />
       </div>
       <Loader type="pacman" />
     </>
